f09567e19d3e879f20d1b53a28ad92499065f2cc
chris <chrishethertonweb@gmail.com>
2022-10-07 10:08:58 +0200

    added switch state for third page

--- js/index.js
+++ js/index.js
@@ -42,7 +42,14 @@ function nextDiv(element) {
   let checkBirthDate = document.getElementsByName('birthDate')[0].checkValidity()
   let checkNationality = document.getElementsByName('nationality')[0].checkValidity()
 
+  let checkUserName = document.getElementsByName('username')[0].checkValidity()
+  let checkPassword = document.getElementsByName('password')[0].checkValidity()
+  let checkRepeatPassword = document.getElementsByName('password-repeat')[0].checkValidity()
+  let checkImageFile = document.getElementsByName('imageFile')[0].checkValidity()
+
   let fatherDiv = element.parentNode.id;
+
+  //     SWITCH STATE FOR SECOND PAGE
 
   switch (fatherDiv) {
     case 'personalDiv':
@@ -60,6 +67,28 @@ function nextDiv(element) {
         alert('You have invalid entries')
       }
 
+      break;
+
+    default:
+      break;
+  }
+
+  //      SWITCH STATE FOR THIRD PAGE
+  switch (fatherDiv) {
+    case 'profileDiv':
+
+      if (checkUserName &&
+        checkPassword &&
+        checkRepeatPassword &&
+        checkImageFile) {
+
+        element.parentNode.style.display = 'none'
+        document.getElementById('resultDiv').style.display = 'none';
+        document.getElementById('profileDiv').style.display = 'none';
+        document.getElementById('resultDiv').style.display = 'block';
+      } else {
+        alert('You have invalid entries')
+      }
       break;
 
     default:
